066d4ba1a76216bd75e4689c1ef7384402e90f2a
MarVinCan <marcus.v.e.candido@gmail.com>
2022-03-29 20:42:53 -0300

    Reference method com método não estático

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -24,6 +24,9 @@ public class Product {
 	public static void staticPriceProduct(Product p) {
 		p.setPrice(p.getPrice() * 1.1);
 	}
+	public void nonStaticPriceProduct() {
+		price = price * 1.1;
+	}
 	@Override
 	public String toString() {
 		return name +  String.format(", %.2f",price);
